b84f9bfd98d80aa1f64ca55ff2366a9e027cef99
Raksha1998 <shreerakshadesai@gmail.com>
2023-06-05 21:25:20 +0200

    Created DocuModel Class and initialized variables required

---
+++ DocuWebApp/DocuWebApp/Model/DocuModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DocuWebApp.Model
+{
+	public class DocuModel
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FileType { get; set; }
+        public string FilePath { get; set; }
+        
+    }
+}
+
