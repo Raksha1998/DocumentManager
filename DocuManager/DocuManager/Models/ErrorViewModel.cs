b2b3e482c475d13f4060ccd404cf518e2c7ad266
Raksha1998 <shreerakshadesai@gmail.com>
2023-06-15 10:36:46 +0200

    Created DocuManager MVC project

---
+++ DocuManager/DocuManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DocuManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
