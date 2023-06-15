98927b8aaf012191da931c78eca203d3615629eb
Raksha1998 <shreerakshadesai@gmail.com>
2023-06-15 11:02:50 +0200

    Added Document Model

---
+++ DocuManager/DocuManager/Models/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DocuManager.Models
+{
+    public class Document
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string TypeIcon { get; set; }
+        public string PreviewImage { get; set; }
+        public DateTime UploadDateTime { get; set; }
+        public int DownloadCount { get; set; }
+        public int Id { get; set; }
+    }
+}
+
